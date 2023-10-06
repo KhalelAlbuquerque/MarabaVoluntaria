19c04a0b2e4e055c7ff21717c284287b9f3fe014
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-05 23:35:16 -0300

    feat: resp mobile login

--- front/src/app/login/page.jsx
+++ front/src/app/login/page.jsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import Header from '@/components/Header/Header.jsx'
 import Footer from '@/components/Footer/Footer.jsx'
+import InputPrimario from '@/components/Input/InputPrimario'
+import { AiOutlineMail } from 'react-icons/ai'
+import  { AiOutlineLock } from 'react-icons/ai'
 
 export default function Login() {
   return (
@@ -8,6 +11,33 @@ export default function Login() {
       <header>
         <Header/>
       </header>
+      <main>
+        <div>
+          <h1 className='text-center text-gray-700 font-semibold text-4xl mb-8'>
+            Tela login
+          </h1>
+        </div>
+        <div className='p-4 flex flex-col gap-4'>
+          <InputPrimario
+          type="text"
+          name="email"
+          placeholder="Email"
+          icon={AiOutlineMail}
+          />
+          <InputPrimario
+          type="password"
+          name="password"
+          placeholder="Senha"
+          icon={AiOutlineLock}
+          />
+          <a className='px-2 text-gray-500 text-sm'>
+            Esqueceu a senha?
+          </a>
+          <button className='w-full rounded-3xl py-3 text-white bg-sky-300 hover:bg-green-300'>
+            Login
+          </button>
+        </div>
+      </main>
       <footer className='fixed bottom-0 w-full'>
        <Footer/>
       </footer>
--- front/src/components/Input/InputPrimario.jsx
+++ front/src/components/Input/InputPrimario.jsx
@@ -1,6 +1,6 @@
 export default function InputPrimario({ type, name, icon: Icon, placeholder }){
     return (
-      <div className="flex gap-4 bg-white items-center rounded-3xl px-3 pl-6 py-3 sm:w-[300px] lg:mx-auto lg:w-[410px] xl:w-[650px]">
+      <div className="flex gap-4 bg-white border-2 border-gray-500 items-center rounded-3xl px-3 pl-6 py-3 sm:w-[300px] lg:mx-auto lg:w-[410px] xl:w-[650px]">
         <Icon className="flex text-[24px] text-primaryLight-500" />
         <input
           type={type}
