55e78560e887cad2a1da5a35801ea899f4ece584
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-06 15:42:34 -0300

    feat: tela cadastro e adicionando imagem nas telas

--- front/src/app/cadastro/page.jsx
+++ front/src/app/cadastro/page.jsx
@@ -1,6 +1,11 @@
-import Image from 'next/image'
 import Header from '@/components/Header/Header.jsx'
 import Footer from '@/components/Footer/Footer.jsx'
+import InputPrimario from '@/components/Input/InputPrimario.jsx'
+import { AiOutlineMail, AiOutlineLock } from 'react-icons/ai'
+import { GoPerson } from 'react-icons/go'
+import Image from 'next/image'
+
+import cadastro from './cadastro.png'
 
 export default function Cadastro() {
   return (
@@ -8,6 +13,47 @@ export default function Cadastro() {
       <header>
         <Header/>
       </header>
+      <main className='flex justify-around items-center'>
+        <div className='max-[1034px]:hidden flex'>
+          <Image
+            src={cadastro}
+            alt='Imagem de cadastro'
+            width={500}
+            height={500}
+            layout='intrinsic'
+          />
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+          <div>
+            <h1 className='text-center text-gray-700 font-semibold text-4xl mb-8'>
+              Tela Cadastro
+            </h1>
+          </div>
+          <div className='p-4 flex flex-col gap-4'>
+            <InputPrimario
+            type="text"
+            name="name"
+            placeholder="Nome"
+            icon={GoPerson}
+            />
+            <InputPrimario
+            type="text"
+            name="email"
+            placeholder="Email"
+            icon={AiOutlineMail} 
+            />
+            <InputPrimario
+            type="password"
+            name="password"
+            placeholder="Senha"
+            icon={AiOutlineLock}
+            />  
+            <button className='w-full rounded-3xl py-3 text-white bg-sky-300 hover:bg-green-300'>
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </main>
       <footer className='fixed bottom-0 w-full'>
        <Footer/>
       </footer>
--- front/src/app/login/page.jsx
+++ front/src/app/login/page.jsx
@@ -4,6 +4,7 @@ import Footer from '@/components/Footer/Footer.jsx'
 import InputPrimario from '@/components/Input/InputPrimario'
 import { AiOutlineMail } from 'react-icons/ai'
 import  { AiOutlineLock } from 'react-icons/ai'
+import login from './login.png'
 
 export default function Login() {
   return (
@@ -11,31 +12,39 @@ export default function Login() {
       <header>
         <Header/>
       </header>
-      <main>
-        <div>
-          <h1 className='text-center text-gray-700 font-semibold text-4xl mb-8'>
-            Tela login
-          </h1>
+      <main className='flex justify-around items-center'>
+        <div className='max-[1034px]:hidden flex'>
+          <Image
+            src={login}
+            alt='Imagem de cadastro'
+            width={500}
+            height={500}
+            layout='intrinsic'
+          />
         </div>
-        <div className='p-4 flex flex-col gap-4'>
-          <InputPrimario
-          type="text"
-          name="email"
-          placeholder="Email"
-          icon={AiOutlineMail}
-          />
-          <InputPrimario
-          type="password"
-          name="password"
-          placeholder="Senha"
-          icon={AiOutlineLock}
-          />
-          <a className='px-2 text-gray-500 text-sm'>
-            Esqueceu a senha?
-          </a>
-          <button className='w-full rounded-3xl py-3 text-white bg-sky-300 hover:bg-green-300'>
-            Login
-          </button>
+        <div className='flex flex-col justify-center items-center'>
+          <div>
+            <h1 className='text-center text-gray-700 font-semibold text-4xl mb-8'>
+              Tela Login
+            </h1>
+          </div>
+          <div className='p-4 flex flex-col gap-4'>
+            <InputPrimario
+            type="email"
+            name="email"
+            placeholder="Email"
+            icon={AiOutlineMail} 
+            />
+            <InputPrimario
+            type="password"
+            name="password"
+            placeholder="Senha"
+            icon={AiOutlineLock}
+            />  
+            <button className='w-full rounded-3xl py-3 text-white bg-sky-300 hover:bg-green-300'>
+              Login
+            </button>
+          </div>
         </div>
       </main>
       <footer className='fixed bottom-0 w-full'>
