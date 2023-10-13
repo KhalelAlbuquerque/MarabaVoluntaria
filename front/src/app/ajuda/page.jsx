42ed5dbc4de6281adf6711fe1dd206810cb4294f
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-13 19:55:50 -0300

    feat: Page Ajuda

---
+++ front/src/app/ajuda/page.jsx
@@ -0,0 +1,23 @@
+import CardVaga from "@/components/Card/CardVaga";
+import Header from "@/components/Header/Header";
+import './styles.css'
+
+export default function Ajuda(){
+    return(
+        <div>
+            <header>
+                <Header/>
+            </header>
+            <main className="container">
+                <div>
+                    <h1>FODASE</h1>
+                    <p>asidhashdiaihdaihsdadasd</p>
+                </div>
+                <div>
+                    <h1>ASDASD</h1>
+                    <p>FODasdasdhuiasudghauda</p>
+                </div>
+            </main>
+        </div>
+    )
+}
