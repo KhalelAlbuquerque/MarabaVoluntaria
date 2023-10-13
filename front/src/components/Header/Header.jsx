b58fe96f937335f7102ef6645b28563b37718fce
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-13 11:27:35 -0300

    feat: Input SignIn

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -2,16 +2,17 @@ import InputPrimario from "../Input/InputPrimario.jsx";
 import { FaSearch } from "react-icons/fa";
 import { FaHandshake } from "react-icons/fa";
 import SideBar from "./SideBar/SideBar.jsx";
+import InputSignIn from "../Input/InputSignIn.jsx";
 
 export default function Header(){
     return (
-        <header className="flex min-[352px]:px-4 px-8 justify-between lg:px-28 py-6 justify- items-center bg-sky-300">
+        <header className="flex max-[433px]:px-0 min-[520px]:px-8 justify-between lg:px-28 py-6 justify- items-center bg-sky-300 min-[433px]:px-2">
             <div className="flex lg:hidden">
                 <SideBar/>
             </div>
             <div className="flex gap-8 items-center">
                 <FaHandshake className="text-6xl max-[1024px]:hidden"/>
-                <InputPrimario type="text" name="search" icon={FaSearch} placeholder="Pesquisar..."/>
+                <InputSignIn type="text" name="search" icon={FaSearch} placeholder="Pesquisar..."/>
             </div>
             <div className="flex gap-4 items-center font-semibold max-[1024px]:hidden">
                 <a className="text-sky-950 hover:text-gray-500 hover:underline cursor-pointer transition-colors duration-300">Home</a>
