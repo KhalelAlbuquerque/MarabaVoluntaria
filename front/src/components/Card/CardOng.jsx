69bf99a62d1d730db7321e8a4dee544b82737781
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-16 19:11:29 -0300

    feat: CardOng

---
+++ front/src/components/Card/CardOng.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import imageCard from './imgs/crianca-card.webp'
+
+export default function CardOng({localizacao, nomeONG, descONG}){
+    return (
+        <div className="flex flex-col w-60 border-2 h-[350px] rounded-lg shadow-xl">
+            <div>
+                <Image
+                src={imageCard}
+                alt="Imagem do card"
+                className="rounded-t-lg rounded-b-xl"
+                />
+            </div>
+            <div className=" mt-2 px-2 flex flex-col gap-1">
+                <div className="text-orange-700">
+                    <h1>{localizacao}</h1>
+                </div>
+                <div>
+                    <h1 className="font-semibold">{nomeONG}</h1>
+                </div>
+                <div className="text-sm">
+                    <p>{descONG}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
