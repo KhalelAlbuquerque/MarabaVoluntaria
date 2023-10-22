9272fcd5aeb02acbea1f965dd0f6efc8d6cfe14a
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-10-22 16:36:17 -0300

    update: CadastroOng + validation

--- front/src/app/(dinamics)/(vaga)/cadastro_ong/page.jsx
+++ front/src/app/(dinamics)/(vaga)/cadastro_ong/page.jsx
@@ -1,12 +1,10 @@
 'use client'
 import InputPrimario from '@/components/Input/InputPrimario.jsx'
 import { AiOutlineMail, AiOutlineLock } from 'react-icons/ai'
+import { VscOrganization } from 'react-icons/vsc'
 import { FiPhone } from 'react-icons/fi'
-import { GoPerson } from 'react-icons/go'
-import Image from 'next/image'
+import { SiWorldhealthorganization } from 'react-icons/si'
 import { useState,useEffect } from 'react'
-
-import cadastro from '@/app/(main)/cadastro/cadastro.png'
 import InputSignIn from '@/components/Input/InputSignIn'
 
 import { useRouter } from 'next/navigation'
@@ -16,29 +14,42 @@ export default function CadastroOng() {
   const router = useRouter()
 
   const [user,setUser] = useState('')
+  const [cnpj,setCnpj] = useState('')
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
   const [number,setNumber] = useState('')
+  const [descricao, setDescricao] = useState('')
+  const [sobre, setSobre] = useState('')
   const [alertPass, setAlertPass] = useState(false)
   const [alertNumber, setAlertNumber] = useState(false)
   const [alertUser, setAlertUser] = useState(false)
   const [alertEmail, setAlertEmail] = useState(false)
+  const [alertCnpj, setAlertCnpj] = useState(false)
+  const [alertDescricao, setAlertDescricao] = useState(false)
+  const [alertSobre, setAlertSobre] = useState(false)
 
   var RegExp = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
+  var RegExCnpj = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/
 
   useEffect(() => {
     const timeoutPass = setTimeout(() => setAlertPass(false), 2000);
     const timeoutNumber = setTimeout(() => setAlertNumber(false), 2000);
     const timeoutUser = setTimeout(() => setAlertUser(false), 2000);
     const timeoutEmail = setTimeout(() => setAlertEmail(false), 2000);
+    const timeoutCnpj = setTimeout(() => setAlertCnpj(false), 2000);
+    const timeoutDescricao = setTimeout(() => setAlertDescricao(false), 2000);
+    const timeoutSobre = setTimeout(() => setAlertSobre(false), 2000);
 
     return () => {
       clearTimeout(timeoutPass);
       clearTimeout(timeoutNumber);
       clearTimeout(timeoutUser);
       clearTimeout(timeoutEmail);
+      clearTimeout(timeoutCnpj);
+      clearTimeout(timeoutDescricao);
+      clearTimeout(timeoutSobre);
     };
-  }, [alertPass, alertNumber, alertUser, alertEmail]);
+  }, [alertPass, alertNumber, alertUser, alertEmail, alertCnpj, alertDescricao, alertSobre]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -46,7 +57,11 @@ export default function CadastroOng() {
     verifyPass(password)
     verifyUser(user)
     verifyEmail(email)
+    verifyCnpj(cnpj)
+    verifyDescricao(descricao)
+    verifySobre(sobre)
     redirect()
+    console.log(`Desc: ${descricao} Sobre: ${sobre}`)
   }
 
   function verifyPass(password) {
@@ -72,8 +87,8 @@ export default function CadastroOng() {
   }
 
   function redirect() {
-    if (verifyNumber(number) && verifyPass(password) && verifyUser(user) && verifyEmail(email)) {
-      router.push('/login')
+    if (verifyNumber(number) && verifyPass(password) && verifyUser(user) && verifyEmail(email) && verifyCnpj(cnpj) && verifyDescricao(descricao) && verifySobre(sobre)) {
+      router.push('/login_ong')
     }
   }
 
@@ -99,28 +114,76 @@ export default function CadastroOng() {
     }
   }
 
+  function verifyCnpj(cnpj){
+    if (RegExCnpj.test(cnpj)) {
+      setCnpj(cnpj)
+      return true
+    } else {
+      setCnpj('')
+      setAlertCnpj(true)
+      return false
+    }
+  }
+
+  function verifyDescricao(descricao) {
+    if (descricao.length > 10) {
+      setDescricao(descricao)
+      return true
+    } else {
+      setDescricao('')
+      setAlertDescricao(true)
+      return false
+    }
+  }
+
+  function verifySobre(sobre) {
+    if (sobre.length > 10) {
+      setSobre(sobre)
+      return true
+    } else {
+      setSobre('')
+      setAlertSobre(true)
+      return false
+    }
+  }
+
+  let descricaoOng = 'Somos uma instituição que atua a 51 anos na Ceilândia-DF, com atendimento gratuito a crianças e para a comunidade por meio de cursos profissionalizantes.'
+  let sobreOng = 'O Centro Social Luterano Cantinho do Girassol foi fundado em 16 de março de 1972 (oFIcialmente em 20 de maio de 1974), em Ceilândia – Distrito Federal, mantido pela Comunidade Evangélica de ConFIssão Luterana de Brasília, como um Centro de auxílio a crianças e adolescentes de baixa renda residentes na região.'
+
   return (
-    <main className='flex justify-around items-center max-[720px]:flex-col max-[720px]:mt-12 min-[720px]:mt-10 max-[432px]:mt-2'>
-      <div>
-        <Image
-          src={cadastro}
-          alt='Imagem de cadastro'
-          className='max-[1025px]:w-[400px] max-[768px]:w-[400px] max-[425px]:w-[300px] max-[720px]:w-[300px]'
-          layout='intrinsic'
-        />
+    <main className='flex flex-row-reverse px-20 items-center justify-around max-[720px]:flex-col max-[720px]:mt-12 min-[720px]:mt-10 max-[432px]:mt-2 '>
+      <div className='flex flex-col gap-12'>
+        <div className='flex flex-col gap-2'>
+          <label htmlFor='descircao' className='text-2xl font-semibold'>Descrição da ONG:</label>
+          <textarea className='border-2 p-2 rounded-lg' id='descricao' onChange={({target}) => setDescricao(target.value)} value={descricao} name="descricao" rows="4" cols="50"></textarea>
+          { alertDescricao ? (
+            <p className='text-red-500 text-sm'>
+              Descrição inválida
+            </p>
+          ) : null}
+        </div>
+        <div className='flex flex-col gap-2'>
+          <label htmlFor='sobre' className='text-2xl font-semibold'>Sobre a ONG:</label>
+          <textarea className='border-2 p-2 rounded-lg' id='sobre' onChange={({target}) => setSobre(target.value)} value={sobre} name="sobre" rows="6" cols= "50"></textarea>
+          { alertSobre ? (
+            <p className='text-red-500 text-sm'>
+              Sobre inválido
+            </p>
+          ) : null}
+        </div>
       </div>
       <div className='flex flex-col justify-center items-center'>
         <div>
-          <h1 className='text-center text-gray-700 font-semibold text-4xl mb-8'>
-            Faça seu cadastro
+          <h1 className='text-center text-gray-700 font-semibold text-3xl mb-8'>
+            Faça o cadastro da sua ONG
           </h1>
         </div>
         <form onSubmit={handleSubmit} className='p-4 flex flex-col gap-4'>
           <InputSignIn
           type="text"
           name="nome"
-          placeholder="Nome"
-          icon={GoPerson}
+          placeholder="Nome da sua ONG"
+          icon={SiWorldhealthorganization}
           setValue={setUser}
           value={user}
           />
@@ -130,9 +193,20 @@ export default function CadastroOng() {
             </p>
           ) : null }
           <InputSignIn
+          type="text"
+          name="cnpj"
+          placeholder="CNPJ da sua ONG"
+          icon={VscOrganization}
+          setValue={setCnpj}
+          value={cnpj}
+          />
+          {alertCnpj ? (
+            <p className='text-red-500 text-sm'>CNPJ invalido</p>
+          ) : null}
+          <InputSignIn
           type="email"
           name="email"
-          placeholder="Email"
+          placeholder="Email da ONG"
           icon={AiOutlineMail} 
           setValue={setEmail}
           value={email}
@@ -145,7 +219,7 @@ export default function CadastroOng() {
           <InputSignIn
           type="text"
           name="number"
-          placeholder="Número"
+          placeholder="Número da ONG"
           icon={FiPhone}
           setValue={setNumber}
           value={number}
@@ -158,7 +232,7 @@ export default function CadastroOng() {
           <InputSignIn
           type="password"
           name="password"
-          placeholder="Senha"
+          placeholder="Senha da ONG"
           icon={AiOutlineLock}
           setValue={setPassword}
           value={password}
@@ -169,7 +243,7 @@ export default function CadastroOng() {
             </p>
           ) : null}
           <button className='w-full font-bold py-3 text-white bg-sky-300 hover:bg-green-300 rounded-lg'>
-            Cadastrar
+            Cadastrar ONG
           </button>
         </form>
       </div>
