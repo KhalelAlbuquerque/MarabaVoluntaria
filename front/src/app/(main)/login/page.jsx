a670a0b37a728383b8a49422a8b40b445819ff71
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-11-01 21:14:35 -0300

    feat: resp page login

--- front/src/app/(main)/login/page.jsx
+++ front/src/app/(main)/login/page.jsx
@@ -87,25 +87,25 @@ export default function Login() {
 
   return (
     <div>
-      <div className='max-[720px]:hidden absolute right-1/2 top-1/2 bg-white p-4 border-r-4 shadow-2xl rounded-full'>
+      <div className='max-[840px]:hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 border-r-4 shadow-2xl rounded-full'>
         <MdOutlineKeyboardArrowRight className='text-5xl'/>
       </div>
-      <main className='flex justify-between max-[720px]:flex-col max-[720px]:items-center h-screen 1'> 
-        <div className='flex justify-center items-center w-[47.2%] bg-indigo-100 max-[720px]:bg-white max-[720px]:w-full'>
+      <main className='flex justify-between max-[840px]:justify-start max-[840px]:mt-12 max-[840px]:flex-col max-[840px]:items-center h-screen 1'> 
+        <div className='flex justify-center items-center w-1/2 bg-indigo-100 max-[840px]:bg-white max-[840px]:w-full'>
           <Image
             src={login}
             alt='Imagem de cadastro'
-            className='mx-auto max-[1025px]:w-[400px] max-[768px]:w-[400px] max-[425px]:w-[300px] max-[720px]:w-[300px]'
+            className='mx-auto max-[1025px]:w-[400px] max-[768px]:w-[400px] max-[425px]:w-[300px] max-[840px]:w-[300px]'
             layout='intrinsic'
           />
         </div>
-        <div className='flex flex-col justify-center items-center mx-auto w-[52.8%]'>
+        <div className='flex flex-col justify-center items-center mx-auto w-1/2 max-[840px]:w-full'>
           <div>
             <h1 className='text-center text-gray-700 font-semibold text-4xl mb-8'>
               Faça seu login
             </h1>
           </div>
-          <form onSubmit={handleSubmit} className='p-4 flex flex-col gap-4'>
+          <form onSubmit={handleSubmit} className='p-4 flex flex-col gap-4 max-[337px]:w-full'>
             <InputSignIn
             type="email"
             name="email"
@@ -125,7 +125,7 @@ export default function Login() {
             />
             <input value={"user"} onChange={()=>{}} className='hidden' name='userType'></input>
             {alertPass ? <p className="text-red-500">Senha deve ter no mínimo 8 caracteres</p> : null}
-            <button  onClick={handleSubmit} className='w-full font-bold py-3 text-white bg-sky-300 hover:bg-green-300 rounded-lg'>
+            <button  onClick={handleSubmit} className='w-full font-bold py-3 text-white bg-sky-300 hover:bg-green-300 rounded-lg max-[337px]:w-[250px] max-[337px]:mx-auto'>
               { loading ? <div className='flex gap-5 justify-center'><p>Login</p><Image src={gifLoading} height={20}/></div> : 'Login' }
             </button>
             <p className=' text-center text-gray-700'>Ainda não possui conta? <Link href={"/cadastro"} className="font-bold underline">Cadastre-se</Link></p>
--- front/src/components/Input/InputSignIn.jsx
+++ front/src/components/Input/InputSignIn.jsx
@@ -1,6 +1,6 @@
 export default function InputSignIn({ type, name, icon: Icon, placeholder, setValue, value }){
     return (
-      <div className="flex gap-4 bg-white border-2 border-gray-500 items-center rounded-lg px-3 pl-6 py-3 sm:w-[300px] lg:mx-auto lg:w-[410px] xl:w-[650px] max-[337px]:w-[250px]">
+      <div className="flex gap-4 bg-white border-2 border-gray-500 items-center rounded-lg px-3 pl-6 py-3 max-[337px]:mx-auto min-[1490px]:w-[600px] sm:w-[300px] lg:mx-auto lg:w-[410px] max-[337px]:w-[250px]">
         <Icon className="flex text-[24px] text-primaryLight-500" />
         <input 
           type={type}
